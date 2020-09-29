07c77793f9a16fa74b0d80f6d742c9b3948da8bf
Tomoya Takinami <hjtakinami@gmail.com>
2020-09-29 08:41:56 +0200

    finished

--- basic/raycaster_rotation_cubics/script.js
+++ basic/raycaster_rotation_cubics/script.js
@@ -9,7 +9,7 @@ function init() {
 const scene = new THREE.Scene();
 
 //2. create camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
 //3. create renderer & set antialias(物体の輪郭がギザギザになることを抑える)
