b1a62191b865978361e082a3023acbf22266252c
Tomoya Takinami <hjtakinami@gmail.com>
2020-10-03 13:41:40 +0200

    edit

---
+++ basic/camera_earth_rotation_controls(mamp)/script.js
@@ -0,0 +1,121 @@
+/* https://ics.media/tutorial-three/camera_position/ 
+スライダーによって、カメラの位置と角度を制御できるようにしている
+「Camera Position」(座標)のXYZのそれぞれのスライダーを変更すると、
+カメラの位置が移動し地球の見え方が変わる。
+Camera LookAtのトグルを解除すると、自由にカメラの角度を変更できる　*/
+
+//////////// 1.地球を中心としてカメラを回転させる /////////////
+/* 地球を中心としてカメラが円周上を自動的に移動する、カメラの位置の設定はcameraオブジェクトのpositionプロパティーに数値を代入する */
+
+// waiting for a page loading 
+window.addEventListener('load', init);
+
+function init() {
+
+    //1. set a size
+    const width = 1000;
+    const height = 600;
+    let rot = 0;
+
+    //2. create a renderer
+    const renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector('#myCanvas')
+    });
+    renderer.setSize(width, height);
+
+    //3. create a scene
+    const scene = new THREE.Scene();
+    //4. create a camera
+    const camera = new THREE.PerspectiveCamera(45, width / height);
+
+    //5. create a directional light
+    const directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(1, 1, 1);
+
+    const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+    light.position.set(1, 1, 1);
+    scene.add(directionalLight);
+    scene.add(light);
+
+    //6. create a material
+    // const material = new THREE.MeshStandardMaterial({
+    //     map: new THREE.TextureLoader().load('img/earthmap1k.jpg'),
+    //     side: THREE.DoubleSide
+    // });
+
+    /*//////////////// TEST 画像を読み込む ////////////////
+    画像を使うには読み込み処理を作る必要があります。THREE.TextureLoaderクラスを使う */
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load('img/earthmap1k.jpg');
+
+    // マテリアルにテクスチャーを設定 => マテリアルに画像を使用する
+    const material = new THREE.MeshStandardMaterial({
+        map: texture,
+        side: THREE.DoubleSide
+    });
+    ////////////////////////////////////////////////
+
+
+
+
+    //7. create a geometry 
+    const geometry = new THREE.SphereGeometry(300, 30, 30);
+    
+    //8. create a mesh
+    const earthMesh = new THREE.Mesh(geometry, material);
+    scene.add(earthMesh);
+
+    //9. create a stars
+    createStarField();
+    
+    function createStarField() {
+        // 形状データを作成
+        const geometry = new THREE.Geometry();
+        for (let i = 0; i < 1000; i++) {
+            geometry.vertices.push(
+                new THREE.Vector3(
+                    3000 * (Math.random() - 0.5),
+                    3000 * (Math.random() - 0.5),
+                    3000 * (Math.random() - 0.5)
+                )
+            );
+        }
+        //  create a material 
+        const material = new THREE.PointsMaterial({
+            size: 10,
+            color: 0xffffff
+        });
+
+        // create a mesh
+        const mesh = new THREE.Points(geometry, material);
+        scene.add(mesh);
+    }   //ENDE createStarField
+
+    tick();
+    function tick() {
+        // 毎フレーム角度を0.5度ずつ足していく
+        rot += 0.6;
+
+        // ラジアンに変換する
+        const radian = (rot * Math.PI) / 180;
+
+        // 角度に応じてカメラの位置を設定, カメラの位置の設定はcameraオブジェクトのpositionプロパティーに数値を代入する
+        //動きの演出については、フレーム毎に衛星の配置角度を0.5度ずつ加算し、それをカメラの座標に変換しています。カメラの座標は三角関数（sinとcos）を使って、角度から求めています。1000という値は円の半径
+
+        // view.camera.x = 円周の半径 * Math.sin(角度 * Math.PI / 180);
+        // view.camera.z = 円周の半径 * Math.cos(角度 * Math.PI / 180);
+
+        camera.position.x = 1000 * Math.sin(radian);
+        // camera.position.y = 1000 * Math.cos(radian);
+        camera.position.z = 1000 * Math.cos(radian);
+
+        // 原点方向を見つめる
+        camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+        //rendere
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(tick);
+    } //ENDE function tick()
+
+}   //ENDE init
---
+++ basic/material_and_lighting(mamp)/script.js
@@ -0,0 +1,91 @@
+/* https://ics.media/tutorial-three/material_basic/
+-3Dの見栄えを決定するマテリアルとライティングについて
+マテリアルとは物体の質感設定のこと。3Dで形状を作る際には、見栄えを決める「マテリアル」を指定することで、たとえば、着色や画像・陰影の割り当て、ライティングの反射などを適用できます。
+
+代表的なマテリアルとして「単色塗りのマテリアル」と「画像を使ったマテリアル」の二種類があります。
+どちらもTHREE.MeshStandardMaterialクラスを使用しますが、コンストラクタの引数に色（16進数表記の色）か画像テクスチャを指定するかで自動的に適したマテリアルになります */
+
+// ページの読み込みを待つ
+window.addEventListener('load', init);
+
+function init() {
+
+  // サイズを指定
+  const width = 960;
+  const height = 540;
+
+  // 1.レンダラーを作成 canvasを使用して、html内のid:myCanvasに出力する宣言
+  const renderer = new THREE.WebGLRenderer({
+      canvas: document.querySelector('#myCanvas')
+  });
+  renderer.setSize(width, height);
+
+  // 2.シーンを作成
+  const scene = new THREE.Scene();
+
+  // 3.カメラを作成
+  const camera = new THREE.PerspectiveCamera(60, width / height, 1, 10000);
+  camera.position.set(0, 0, +1000);
+
+  // 4.球体を作成
+  const geometry = new THREE.SphereGeometry(300, 30, 30);
+//   const material = new THREE.MeshStandardMaterial( {color: 0xFF0000} );  //単色塗りのマテリアルの場合
+
+
+  /*//////////////// 画像を読み込む ////////////////
+  画像を使うには読み込み処理を作る必要があります。THREE.TextureLoaderクラスを使う */
+  const loader = new THREE.TextureLoader();
+  const texture = loader.load('img/earthmap1k.jpg');
+
+  // マテリアルにテクスチャーを設定 => マテリアルに画像を使用する
+  const material = new THREE.MeshStandardMaterial({
+    map: texture
+  });
+  ////////////////////////////////////////////////
+  
+  // 5.メッシュを作成
+  const mesh = new THREE.Mesh(geometry, material);
+
+
+  // 6.3D空間にメッシュを追加
+  scene.add(mesh);
+
+  /////////////////// 7.平行光源 ///////////////////
+  /* ライトを作る
+    ライティングがないと陰影はなく画面が真っ暗となり、3Dの質感がわからないため,THREE.DirectionalLightクラスのインスタンスを作成しています。THREE.DirectionalLightクラスは指定した方向からライトを適用でき, これを平行光源といいます。 
+    Three.jsで使用できるライティングには以下の種類があります。
+
+    1.THREE.DirectionalLightクラス
+    平行光源。ライトの位置と方向を指定し平行に到達する光として使用できます。
+
+    2.THREE.AmbientLightクラス
+    環境光。空間全体を照らす光として使用できます。
+*/
+  const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
+  directionalLight.position.set(1, 1, 1);
+
+  /* TEST AmbientLight */
+  const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+
+  // シーンに追加
+  scene.add(directionalLight);
+  scene.add( light );
+
+  // 毎フレーム時に実行されるループイベントです
+  tick();
+
+  function tick() {
+    
+    // メッシュを回転させる
+    mesh.rotation.y += 0.02;
+    // mesh.rotation.x += 0.02;
+    
+    // レンダリング
+    renderer.render(scene, camera);
+
+    //Animation
+    requestAnimationFrame(tick);
+  }    
+
+
+}; // ENDE init
